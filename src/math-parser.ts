c632ca36cd2ebfd39994fae1c2c2a636409c7f2b
e-kosareva <e.kosareva@mobilunity.com>
2019-08-15 16:57:48 +0300

    path parser completed

--- src/math-parser.ts
+++ src/math-parser.ts
@@ -1,88 +1,83 @@
-const reg1 = /[0-9\.]*/;
-const reg2 = /[\+,\-,\*,\/]/;
-const init = '2 + 33 / 4 + 1';
-const priority = ['*', '/', '+', '-'];
+const MathParser = {
+    rules: {
+        number: /[0-9\.]*/,
+        operator: /[\+,\-,\*,\/]/,
+    },
+    priority: ['*', '/', '+', '-'],
+    operationsMap: (self) => new Map([
+        ['*', self.multiply],
+        ['/', self.divide],
+        ['+', self.sum],
+        ['-', self.subtract],
+    ]),
+    sum: (a, b) => +a + +b,
+    multiply: (a, b) => +a * +b,
+    divide: (a, b) => +a / +b,
+    subtract: (a, b) => +a - +b,
+    Token: function (type, val) {
+        this.type = type.toString();
+        this.value = val.toString();
+    },
+    tokenize: function (input) {
+        const tokens = [];
+        let inputCopy = input.slice().replace(/\s+/g, '');
+        while (inputCopy.length) {
 
-let initCopy = init.slice();
-const res: any = [];
-while (initCopy.length) {
-    if (initCopy.match(reg1)![0].length) {
-        const matchNumber = initCopy.match(reg1);
-        res.push({
-            type: 'N',
-            value: matchNumber![0]
+            let matchVal = null;
+            let mathType = null;
+            if (inputCopy.match(this.rules.number)[0].length) {
+                matchVal = inputCopy.match(this.rules.number)[0];
+                mathType = 'N'
+            } else if (inputCopy.match(this.rules.operator)[0].length) {
+                matchVal = inputCopy.match(this.rules.operator)[0];
+                mathType = 'O'
+            }
+
+            tokens.push(new this.Token(mathType, matchVal));
+            inputCopy = inputCopy.replace(matchVal, '');
+        }
+        return tokens;
+    },
+    evaluate: function (input) {
+        const tokens = this.tokenize(input);
+        const ast = this.buildAST(tokens);
+        let res = 0;
+
+        while (ast.length > 1) {
+            const ind = ast.findIndex(val => val.toString().match(this.rules.operator));
+            const firstN = ast[ind - 2];
+            const lastN = ast[ind - 1];
+            res = +this.operationsMap(this).get(ast[ind]).apply(this, [firstN, lastN]);
+            ast.splice(ind - 2, 3, res);
+        }
+
+        return res;
+    },
+    buildAST: function (tokens) {
+        let nStack: any[] = [];
+        const oStack: any[] = [];
+
+        tokens.forEach((token) => {
+            if (token.type === 'N') {
+                nStack.push(token.value);
+            } else if (token.type === 'O') {
+                if (oStack.length) {
+                    const lastOPriorityIndex = this.priority.findIndex(o => o === oStack[oStack.length - 1]);
+                    const oPriorityIndex = this.priority.findIndex(o => o === token.value);
+                    if (oPriorityIndex > lastOPriorityIndex) {
+                        nStack.push(oStack.pop());
+                    }
+                }
+                oStack.push(token.value);
+            }
         });
-        initCopy = initCopy.replace(matchNumber![0], '').replace(/\s/g, '');
-    } else if (initCopy.match(reg2)![0].length) {
-        const matchOperator = initCopy.match(reg2);
-        res.push({
-            type: 'O',
-            value: matchOperator![0]
-        });
-        initCopy = initCopy.replace(matchOperator![0], '').replace(/\s/g, '');
-    } else {
-        initCopy = initCopy.substring(1, initCopy.length);
+
+        oStack.reverse();
+        nStack = [...nStack, ...oStack];
+
+        return nStack;
     }
-}
-console.log(res);
+};
 
-// =====================================================================================================================
-
-// function Token(type, value) {
-//     this.type = type;
-//     this.value = value
-// }
-//
-// function tokenize(str) {
-//     var result = [];
-//     str.replace(/\s+/g, "");
-//     str = str.split("");
-//     str.forEach(function (char, idx) {
-//         if (isDigit(char)) {
-//             result.push(new Token("Literal", char));
-//         } else if (isLetter(char)) {
-//             result.push(new Token("Variable", char));
-//         } else if (isOperator(char)) {
-//             result.push(new Token("Operator", char));
-//         } else if (isLeftParenthesis(char)) {
-//             result.push(new Token("Left Parenthesis", char));
-//         } else if (isRightParenthesis(char)) {
-//             result.push(new Token("Right Parenthesis", char));
-//         } else if (isComma(char)) {
-//             result.push(new Token("Function Argument Separator", char));
-//         }
-//     });
-//     return result;
-// }
-//
-// function isComma(ch) {
-//     return (ch === ",");
-// }
-//
-// function isDigit(ch) {
-//     return /\d/.test(ch);
-// }
-//
-// function isLetter(ch) {
-//     return /[a-z]/i.test(ch);
-// }
-//
-// function isOperator(ch) {
-//     return /\+|-|\*|\/|\^/.test(ch);
-// }
-//
-// function isLeftParenthesis(ch) {
-//     return (ch === "(");
-// }
-//
-// function isRightParenthesis(ch) {
-//     return (ch == ")");
-// }
-//
-// console.log(tokenize('2 + 3, 4a + 1, 5x+ (2y), 11 + sin(20.4)'));
-//
-// var tokens = tokenize("89sin(45) + 2.2x/7");
-// tokens.forEach(function (token, index) {
-//     console.log(index + "=> " + token.type + "(" + token.value + ")"
-// :
-// });
+console.log(MathParser.evaluate('2 + 44 / 4 + 1')); // 14
+console.log(MathParser.evaluate('2 + 44 / 4 + 1 + 2 * 4')); // 22
